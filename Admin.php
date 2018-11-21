d61cdc1d7ee432144e189b5b44493060fd00633b
Kazi Tanveer <43889065+Tanveer16-31347-1@users.noreply.github.com>
2018-11-21 12:50:08 +0600

    Update Admin.php
    CSS in Admin

--- Admin.php
+++ Admin.php
@@ -1,16 +1,47 @@
 <!DOCTYPE html>
+
+<?php
+session_start();
+if(isset($_SESSION["login"]) && $_SESSION["login"]=="yes")
+{
+	
+}
+else
+{
+	echo "Invalid user";
+	header("Location:lp.php?error=RESTRICTED");
+}
+?>
+
 <html>
-	<body bgcolor=cyan   >>
+	<head><title>Crux Solver-ADMIN</title></head>
+	<body bgcolor=cyan>
+	<?php
+
+
+echo "<b style='color:purple'><img src='man.png' alt='LogOut' height='30' width='30'> ". strtoupper($_SESSION["username"]."</b>");
+
+echo " <a href='lout.php'><img src='logout.png' alt='LogOut' height='25' width='25'></a>";
+
+if($_SESSION["username"]=="rifat")
+{echo " <a href='admin.php'><img src='adminpanel.png' alt='admin panel' height='25' width='25'></a>";}
+?>
+
+
+<body bgcolor=cyan>
+<br><br>
+	
+	
 	<table align= "center" style="border2">
 	<tr>
 	<td>
 	
-      <h1 style="text-align:center">Admin Panel</h1><br>
+      <h1 style="color:rgb(192,192,192);text-align:center">Admin Panel</h1><br>
 	  
 	<form name="openfile" method="post" action="">
         Open File: 
-		<input type="text" name="filename" value="">
-        <input type="submit" name="submitopen" value="Open"><br/><br/>
+		<input style="background-color:rgb(191,255,255)" type="text" name="filename" value="" >
+        <input type="submit" style="background-color:rgb(128,255,128)" name="submitopen" value="Open"><br/><br/>
 
     
     File Data:<br/>
@@ -35,19 +66,18 @@
 		</form>
 	  <br>
 	  Report:<br>
-		<textarea CLASS="resize" rows="5" cols="100">
+
+		
+		<textarea placeholder="Enter Report Description" style="background-color:rgb(191,255,255);height: 150; width:1457;"></textarea> <br>
          
-		</textarea><br>
 		<input type="Button" name="Submit" value="Delete"><br><br>
-	   
-	  Enter Any Text:<br>
-		<textarea CLASS="resize" rows="5" cols="100">
-         
-		</textarea><br>
+	  Remove: <br> 
+		<textarea placeholder="Enter User Description" style="background-color:rgb(191,255,255);height: 150; width:1457;"></textarea> <br>
+
 		<input type="Button" name="Submit" value="Remove"><br><br>
 		
-	  Enter Any Text:<br>
-		<textarea CLASS="resize" rows="5" cols="100">
+	  Block:<br>
+		<textarea placeholder="Enter User Description" style="background-color:rgb(191,255,255);height: 150; width:1457;"></textarea> <br>
          
 		</textarea><br>
 		<input type="Button" name="Submit" value="Block"><br><br>
@@ -64,5 +94,6 @@
 	</td>
 	</tr>
 	</table>
+
 	</body>
 </html>
